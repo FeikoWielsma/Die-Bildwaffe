debc59b8adde96289bb3c4825068cc58e37aec9c
FeikoWielsma <feiko.wielsma@student.hu.nl>
2015-04-21 11:40:49 +0200

    Added better values for intensity to grayscale

--- source/ExternalDLL/ExternalDLL/StudentIntensityConvertor.cpp
+++ source/ExternalDLL/ExternalDLL/StudentIntensityConvertor.cpp
@@ -6,7 +6,11 @@ void StudentIntensityConvertor::doAlgorithm(const RGBImage& input, IntensityImag
 	dst.set(input.getWidth(), input.getHeight());
 
 	for (int i = 0; i < input.getWidth() * input.getHeight(); ++i){
-		Intensity in = Intensity((input.getPixel(i).r + input.getPixel(i).g + input.getPixel(i).b) / 3);
+		Intensity in = Intensity(
+			(input.getPixel(i).r * RtoGrayScale) + 
+			(input.getPixel(i).g * GtoGrayScale) +
+			(input.getPixel(i).b * BtoGrayScale)
+		);
 		dst.setPixel(i, in);
 	}
 }
--- source/ExternalDLL/ExternalDLL/StudentIntensityConvertor.h
+++ source/ExternalDLL/ExternalDLL/StudentIntensityConvertor.h
@@ -8,5 +8,10 @@ class StudentIntensityConvertor : public Algorithm<RGBImage, IntensityImage>
 {
 public:
 	void doAlgorithm(const RGBImage &input, IntensityImage &dst) override;
+private:
+	const double RtoGrayScale{ 0.2989 };
+	const double GtoGrayScale{ 0.5870 };
+	const double BtoGrayScale{ 0.1140 };
+
 };
 
