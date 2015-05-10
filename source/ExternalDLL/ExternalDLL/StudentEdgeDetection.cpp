8a59db3be117dcbc8966dcb68caa9f5ec75a2bd6
bart-met-de-luxe-achternaam <bart.muelders@student.hu.nl>
2015-05-10 11:26:25 +0200

    algoritme verbeterd
    maar werkt nog steeds niet goed

--- source/ExternalDLL/ExternalDLL/StudentEdgeDetection.cpp
+++ source/ExternalDLL/ExternalDLL/StudentEdgeDetection.cpp
@@ -1,20 +1,19 @@
 #include "StudentEdgeDetection.h"
 #include <iostream>
 
-const int mask[3][3] = { { 4, 7, 2 }, { 2, 7, 3 }, { 1, 7, 1 } };
 
 void StudentEdgeDetection::doAlgorithm(const IntensityImage &input, IntensityImage &dst) {
-	neighborhood.add(-1, -1, 1);
+	neighborhood.add(-1, -1, 0);
 	neighborhood.add(0, -1, 1);
-	neighborhood.add(1, -1, 1);
+	neighborhood.add(1, -1, 0);
 
 	neighborhood.add(-1, 0, 1);
-	//neighborhood.add(0, 0, 1);
+	neighborhood.add(0, 0, -4);
 	neighborhood.add(1, 0, 1);
 
-	neighborhood.add(-1, 1, 1);
+	neighborhood.add(-1, 1, 0);
 	neighborhood.add(0, 1, 1);
-	neighborhood.add(1, 1, 1);
+	neighborhood.add(1, 1, 0);
 
 	int neighborEdge = neighborhood.getSize() / 2;
 
@@ -22,22 +21,23 @@ void StudentEdgeDetection::doAlgorithm(const IntensityImage &input, IntensityIma
 
 	for (int x = neighborEdge; x < input.getWidth() - neighborEdge; ++x){
 		for (int y = neighborEdge; y < input.getHeight() - neighborEdge; ++y){
+
 			float newIntensity = 0;
 			for (int i = -neighborEdge; i <= neighborEdge; ++i)
 			{
 				for (int j = -neighborEdge; j <= neighborEdge; ++j)
 			 	{
-					if (i != 0 && j != 0){
-						newIntensity += input.getPixel(x + i, y + j) * neighborhood.get(i, j);
-					}
+					newIntensity += input.getPixel(x + i, y + j) * neighborhood.get(i, j);
 			 	}
 			}
-			newIntensity /= neighborhood.getSize() * neighborhood.getSize() - 1;
-			if (newIntensity < 75){
-				dst.setPixel(x, y, 255);
+
+			newIntensity /= neighborhood.getSize() * neighborhood.getSize()-4;
+
+			if (newIntensity < 7){
+				dst.setPixel(x, y, 0);
 			}
 			else{
-				dst.setPixel(x, y, 0);
+				dst.setPixel(x, y, 255);
 			}
 		}
 	}
